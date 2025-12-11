700c61c8d93d740ce3d8fb0c66734afee8284013
EmmanuelMose <emmanuelmose806@gmail.com>
2025-12-11 09:02:46 +0300

    9 commit

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css';
+//import logo1 from '../../../frontend/src/assets/images/logo1';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +17,7 @@ const Navbar = () => {
         <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
           <li>
             <a href="#" className="logo-link">
-              <img src="/SDA_logo.png" alt="SDA Logo" className="logo" />
+              <img src="" alt="SDA Logo" className="logo" />
               <span>MMUSDA</span>
             </a>
           </li>
