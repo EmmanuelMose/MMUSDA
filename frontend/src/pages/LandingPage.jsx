211efd5258e7f46cd701b8e35707ad25ed58b54f
EmmanuelMose <emmanuelmose806@gmail.com>
2025-12-13 20:30:36 +0300

    63 commit

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,16 +3,16 @@ import Navbar from "../../../frontend/src/components/navbar/Navbar";
 import Home from "../../../frontend/src/components/home/Home";
 import Main from "../../../frontend/src/components/main/Main";
 import Sermons from "../../../frontend/src/components/sermons/Sermons";
-import Header from "../components/header/Header";
+import Header from "../../../frontend/src/components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Navbar />
       <Home />
       <Main />
       <Sermons />
-      <Header />
     </div>
   );
 }
